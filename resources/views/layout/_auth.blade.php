d30ebf6f3bac5d322ca14eb21c0905716b24c584
arnaro@123 <hiamarnab@gmail.com>
2024-03-07 20:26:43 +0530

    erg

--- resources/views/layout/_auth.blade.php
+++ resources/views/layout/_auth.blade.php
@@ -19,16 +19,16 @@
                     <!--end::Wrapper-->
                 </div>
                 <!--end::Form-->
-
+                
                 <!--begin::Footer-->
                 <div class="d-flex flex-center flex-wrap px-5">
                     <!--begin::Links-->
                     <div class="d-flex fw-semibold text-primary fs-base">
-                        <a href="#" class="px-5" target="_blank">Terms</a>
+                        <a href="#" class="px-5" target="_blank">Terms & Conditions</a>
 
-                        <a href="#" class="px-5" target="_blank">Plans</a>
-
-                        <a href="#" class="px-5" target="_blank">Contact Us</a>
+                        <a href="#" class="px-5" target="_blank">Privacy Policy
+                        </a>
+                        <a href="#" class="px-5" target="_blank">Get Support
                     </div>
                     <!--end::Links-->
                 </div>
@@ -42,28 +42,28 @@
                 <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
                     <!--begin::Logo-->
                     <a href="{{ route('dashboard') }}" class="mb-12">
-                        <img alt="Logo" src="{{ image('logos/custom-1.png') }}" class="h-60px h-lg-75px"/>
+                        <img alt="Logo" src="{{ image('logos/TIMD logo_RGB.png') }}" class="h-60px h-lg-75px"/>
                     </a>
                     <!--end::Logo-->
 
                     <!--begin::Image-->
-                    <img class="d-none d-lg-block mx-auto w-275px w-md-50 w-xl-500px mb-10 mb-lg-20" src="{{ image('misc/auth-screens.png') }}" alt=""/>
+                    {{-- <img class="d-none d-lg-block mx-auto w-275px w-md-50 w-xl-500px mb-10 mb-lg-20" src="{{ image('misc/auth-screens.png') }}" alt=""/> --}}
                     <!--end::Image-->
 
                     <!--begin::Title-->
                     <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-7">
-                        Fast, Efficient and Productive
+                        TimD PMS - Property Management Simplified
                     </h1>
                     <!--end::Title-->
 
                     <!--begin::Text-->
                     <div class="d-none d-lg-block text-white fs-base text-center">
-                        In this kind of post, <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the blogger</a>
+                        {{-- In this kind of post, <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the blogger</a> --}}
 
-                        introduces a person they’ve interviewed <br/> and provides some background information about
+                         <br/> Ascend your property management journey to new heights with our streamlined solutions,
 
-                        <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the interviewee</a>
-                        and their <br/> work following this is a transcript of the interview.
+                        {{-- <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the interviewee</a> --}}
+                         <br/> optimizing operations for unparalleled efficiency and productivity..
                     </div>
                     <!--end::Text-->
                 </div>
