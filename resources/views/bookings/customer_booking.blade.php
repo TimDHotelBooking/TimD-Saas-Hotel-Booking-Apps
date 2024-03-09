f28e0a3cd0792748916585b89e3cafb4ed86b10c
Parth KaPatel <parth9427@gmail.com>
2024-03-10 04:51:26 +0530

    route issue fixed

--- resources/views/bookings/customer_booking.blade.php
+++ resources/views/bookings/customer_booking.blade.php
@@ -993,7 +993,7 @@
 
                 $.ajax({
                     type: "POST",
-                    url: {{ route("bookings.calculate_total_bill_amount") }},
+                    url: "{{ route("bookings.calculate_total_bill_amount") }}",
                     data:{
                       "room_id" : $("#room_id").val(),
                       "check_in_date" : $("#check_in_date").val(),
